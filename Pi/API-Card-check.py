bd72ba20859823155038306b05a6167cc0d8fd41
rafa-create <rafaelorset@hotmail.fr>
2021-05-17 14:00:27 +0200

    Update API-Card-check.py

--- Pi/API-Card-check.py
+++ Pi/API-Card-check.py
@@ -1,14 +1,15 @@
 import requests
-
-csn="044C40B2616780"
-url = "https://api-public.univ-lorraine.fr/annuaire/cards/check/"+str(csn)
-
+CSN="04 43 59 B2 61 67 80"
+CSN = ''.join(CSN.split())
+#Carte UL active 200 :04 4C 40 B2 61 67 80
+#Carte non trouvée 404 :044C40B26167801
+#Paramètre erroné 400: 044C40B26167801q
+url = "https://api-public.univ-lorraine.fr/annuaire/cards/check/"+str(CSN)
 payload={}
 headers = {
   'X-Gravitee-Api-Key': 'd8cf4b89-af20-4aeb-a143-4898bdd9f4a2',
   'id': 'd8cf4b89af204aeba1434898bdd9f4a2'
 }
-
 response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+print(response.status_code)
+#print(response.text)
