722d4cdfb00f3709f7a09f14217497fdaf31c948
rafa-create <rafaelorset@hotmail.fr>
2021-05-22 01:07:14 +0200

    Update API-CSN-check.py

--- Pi/API-CSN-check.py
+++ Pi/API-CSN-check.py
@@ -4,13 +4,17 @@ import requests
 #Carte non trouvée 404 :04 43 59 B2 61 67 81
 #Paramètre erroné 400: 04 43 59 B2 61 67 8q
 def UL_Card(CSN):#en str
-    CSN = ''.join(CSN.split())
-    url = "https://api-public.univ-lorraine.fr/annuaire/cards/check/"+CSN
-    payload={}
-    headers = {
+    try:
+        CSN = ''.join(CSN.split())
+        url = "https://api-public.univ-lorraine.fr/annuaire/cards/check/"+CSN
+        payload={}
+        headers = {
   'X-Gravitee-Api-Key': 'd8cf4b89-af20-4aeb-a143-4898bdd9f4a2',
   'id': 'd8cf4b89af204aeba1434898bdd9f4a2'
 }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    return response.status_code==200
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.status_code==200
+    except requests.exceptions.RequestException as e :
+        print("pas internet")
+        return True
     #print(response.text)
