ec75cec529408ba9213e62be5c6e189cc7068b5d
rafa-create <rafaelorset@hotmail.fr>
2021-05-21 20:42:21 +0200

    Update api.php

--- Website/api.php
+++ Website/api.php
@@ -1,25 +1,20 @@
 <?php
 $token="jadorelevelo";
-var_dump($_GET);
-var_dump($_SERVER);
- 
-
 
 if(!isset($_SERVER["HTTP_AUTHORIZATION"]) || $_SERVER["HTTP_AUTHORIZATION"] === ''){
-    echo "Il n'y a pas de Token";
+    echo "Unauthorized"; //car Il n'y a pas de Token
     exit;
 }
 
 if($_SERVER["HTTP_AUTHORIZATION"] !== "Bearer ".$token){
-    echo $_SERVER["HTTP_AUTHORIZATION"];
+    echo "Unauthorized"; //car Il n'y a pas le bon Token
     exit;
 
 }
 if(!isset($_GET["Aplaces"]) || $_GET["Aplaces"] === ''){
-    echo "Veuillez renseigner Aplaces";
+    echo "Pas de paramètre";//car il faut renseigner Aplaces
     exit;
 }
 
+echo "Nombre de places actualisé";
 file_put_contents(__DIR__.DIRECTORY_SEPARATOR."places.txt",$_GET["Aplaces"]);
-
-echo "C'est tout bon !";
