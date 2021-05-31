694130b2fbc58da49037081eb9713838aac826e2
rafa-create <rafaelorset@hotmail.fr>
2021-05-31 17:58:09 +0200

    Update 1lock.ino

--- UNO/1lock/1lock.ino
+++ UNO/1lock/1lock.ino
@@ -1,22 +1,16 @@
-#include <MFRC522.h>//carte
-#include <SPI.h>//carte
- 
-#define SS_PIN 10
-#define RST_PIN 9
+
+
 #define LED_G 6 //define green LED pin
 #define LED_R 4 //define red LED
 #define LED_Y 1 //define Yellow LED
 #define BUZZER 5 //buzzer pin
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 int Relay1 = 8;    
 int lock1=3;
 
 void setup() 
 {
-  SPI.begin();//Initiate SPI for card reading
   Serial.begin(57600);   // Initiate a serial communication
-  mfrc522.PCD_Init();   // Initiate MFRC522
   pinMode(LED_G, OUTPUT);
   digitalWrite(LED_G, LOW);
   pinMode(LED_R, OUTPUT);
@@ -27,30 +21,21 @@ void setup()
   noTone(BUZZER);
   pinMode(lock1, INPUT_PULLUP);           
   pinMode(Relay1, OUTPUT); 
-  int etatverrou1=digitalRead(lock1); 
 }
 void loop() {
+  char static msgPi;
   digitalWrite(LED_Y, LOW);
+  int statelock1=digitalRead(lock1);
   if (statelock1==0){//verrou utilisé
     digitalWrite(LED_Y, HIGH); 
     //tone(BUZZER, 600);
     // Look for new cards
-    if ( ! mfrc522.PICC_IsNewCardPresent()) { return;}
-    if ( ! mfrc522.PICC_ReadCardSerial()){return;}
-    String content= "";
-    byte letter;
-    for (byte i = 0; i < mfrc522.uid.size; i++) 
-    { Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
-      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-      content.concat(String(mfrc522.uid.uidByte[i], HEX));}
-    Serial.print("Carte UL et pas CSN courant ?");
-    content.toUpperCase();
+    
     if(Serial.available()>0)
-  { uart_rx_buf=char(Serial.read());
-  Serial.print(uart_rx_buf);
+  { msgPi=char(Serial.read());
+  Serial.print(msgPi);
   Serial.write(Serial.read());
-    if(uart_rx_buf=='True')
+    if(msgPi=='True')
     { digitalWrite(LED_Y, LOW); 
       noTone(BUZZER);
       digitalWrite(LED_Y, LOW);
@@ -77,23 +62,11 @@ void loop() {
       digitalWrite(LED_R, LOW);
       }
     }
-      // Look for new cards
-    if ( ! mfrc522.PICC_IsNewCardPresent()) { return;}
-    if ( ! mfrc522.PICC_ReadCardSerial()){return;}
-    Serial.print("ID card:");
-    String content="";
-    byte letter;
-    for (byte i = 0; i < mfrc522.uid.size; i++) 
-    { Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
-      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-      content.concat(String(mfrc522.uid.uidByte[i], HEX));}
-    content.toUpperCase();
     Serial.print("CSN courant ?");
     if(Serial.available()>0)
-  { uart_rx_buf=char(Serial.read());
+  { msgPi=char(Serial.read());
   Serial.write(Serial.read());
-    if(uart_rx_buf==true)
+    if(msgPi==true)
     { digitalWrite(LED_G, HIGH);
       tone(BUZZER, 500);
       delay(300);
@@ -112,5 +85,7 @@ void loop() {
       digitalWrite(LED_R, LOW);
       Serial.println("Pas de vélo garé désolé");
       }
-      uart_rx_buf = 0;//sait pas à quoi ca sert
+     }
+     }
+     msgPi = 0;
 }
